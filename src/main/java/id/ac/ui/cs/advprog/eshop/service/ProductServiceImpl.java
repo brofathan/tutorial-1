facc0db39f91b6704a504c6efe984c5cdc39244f
Ethanic <emirmfathan@gmail.com>
2024-02-03 06:29:38 +0700

    product service

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -0,0 +1,33 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public Product create(Product product) {
+        productRepository.create(product);
+        return product;
+    }
+
+    @Override
+    public List<Product> findAll() {
+        Iterator<Product> producIterator = productRepository.findAll();
+        List<Product> allProduct = new ArrayList<>();
+        producIterator.forEachRemaining(allProduct::add);
+        return allProduct;
+    }
+    
+}
